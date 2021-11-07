9fa22d0cb6476a17dcff5e190ff4c85ffb7fa399
ayalamaa1 <93656776+ayalamaa1@users.noreply.github.com>
2021-11-07 12:57:50 +0200

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+//Dans le fichier HelloWorld.java
+class HelloWorld {
+   public String bonjour(){ 
+        return "Bonjour tout le monde...";
+   } 
+   // pour être présentée sur la sortie courante toute classe contiendra une méthode de signature  
+   // public String toString(){...} qui surcharge cette même méthode de la classe "Object"
+   public String toString() { 
+        return bonjour();
+     } 
+  }
