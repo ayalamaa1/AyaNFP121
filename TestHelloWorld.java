dc57c2e0ae99c4e24f8eddf3629631b92871f75d
ayalamaa1 <93656776+ayalamaa1@users.noreply.github.com>
2021-11-07 13:02:49 +0200

    Add files via upload

---
+++ TestHelloWorld.java
@@ -0,0 +1,14 @@
+//Dans le fichier TestHelloWorld.java
+public class TestHelloWorld { 
+      // application
+     public static void main (String[] args) { 
+        HelloWorld hello= new HelloWorld();
+
+        System.out.println(hello.toString());  // justifier cette écriture
+
+        System.out.println(hello);  // justifier cette écriture
+
+        System.out.println(new HelloWorld().toString());  // justifier cette écriture
+// les trois instructions retourne le même résultat...
+     } 
+  }
